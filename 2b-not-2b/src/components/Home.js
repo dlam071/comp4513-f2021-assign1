df239426b95df5a68a2b7bb1694f3204fe05203a
Hatoon <halna399@mtroyal.ca>
2021-10-28 17:36:02 -0600

    implemented home component

--- 2b-not-2b/src/components/Home.js
+++ 2b-not-2b/src/components/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = (props) => {
-    return (<p>Home</p>);
+    return (
+        <div>
+            <form className="home">
+                <legend>Play Browser</legend>
+                <label>Title</label>
+                <input type="text" name="title" />
+                 <button > Show Matching Plays</button> {/* add event listener */}
+                <button > Show All Playes</button> {/* add event listener */}
+                
+            </form> 
+
+            <h3>Hero Image Credit</h3>
+        </div>
+
+
+    );
 }
 
 export default Home;
