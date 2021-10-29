79103e4098910bf7ce6634ec1948cc5ea525d100
halna399 <halna399@mtroyal.ca>
2021-10-29 17:30:01 -0600

    commented unnessary classes for home view to test

--- 2b-not-2b/src/App.js
+++ 2b-not-2b/src/App.js
@@ -9,24 +9,24 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p> */}
         
-        <Header />
+        {/* <Header /> */}
         <Home />
-        <PlayBrowser />
-        <PlayDetails />
+        {/* <PlayBrowser />
+        <PlayDetails /> */}
 
-        <a
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
