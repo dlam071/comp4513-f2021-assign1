02ed22b3e43ee8c8909f1a604aa33d2179759e62
halna399 <halna399@mtroyal.ca>
2021-11-04 01:45:27 -0600

    added parent tag and css class woo

--- 2b-not-2b/src/components/Favorites.js
+++ 2b-not-2b/src/components/Favorites.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import "../styles/playbrowser.css"
 const Favorites = (props) => {
-    return (<p>Favorites</p>);
+    return (
+        //think about where to add the toggle button 
+        <aside className="favorites">
+            <h2>Favorites</h2>
+            {/* we vould make a table here or or or or just a title and a button ??? idk */}
+
+        </aside>
+    
+        );
 }
 
 export default Favorites;
