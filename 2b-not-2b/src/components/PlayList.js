291ad8acc3ecd6601ecabe3ed7c1da1c7abdb1a5
halna399 <halna399@mtroyal.ca>
2021-11-04 01:41:33 -0600

    added component basic layout and example of what we want

--- 2b-not-2b/src/components/PlayList.js
+++ 2b-not-2b/src/components/PlayList.js
@@ -1,7 +1,32 @@
 import React from "react";
-
+import "../styles/playbrowser.css"
 const PlayList = (props) => {
-    return (<p>PlayList</p>);
+    return (
+        <section className="playList">
+            <h2>List/Matches</h2>
+            <table>
+                <tr>
+                    <th>Title</th>
+                    <th>year</th>
+                    <th> </th>
+                </tr>
+                <tr>
+                    {/* this is an example */}
+                    <td>Hamlet</td>
+                    <td>1970</td>
+                    <td>
+                        <button>❤️</button>
+                        <button>View</button>
+                    </td>
+                </tr>
+            </table>
+
+
+        </section>
+
+
+
+    );
 }
 
 export default PlayList;
