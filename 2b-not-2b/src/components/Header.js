a781b022743ab06bc10b9d6f478f50c0375c848b
dborc610 <dborc610@mtroyal.ca>
2021-11-04 10:34:50 -0600

    styling done to play browser

--- 2b-not-2b/src/components/Header.js
+++ 2b-not-2b/src/components/Header.js
@@ -1,13 +1,13 @@
 import React from "react";
-import "../styles/playbrowser.css"
+import "../styles/playbrowser.css";
+import About from "./About.js";
+
 const Header = (props) => {
   return (
     <nav className="header">
-       <p>Header</p>
-       <button>About</button>
+      <p>Header</p>
+      <button className="buttonThin buttonSolid">About</button>
     </nav>
-   
-
-  )
+  );
 };
 export default Header;
--- 2b-not-2b/src/components/PlayList.js
+++ 2b-not-2b/src/components/PlayList.js
@@ -1,32 +1,23 @@
 import React from "react";
-import "../styles/playbrowser.css"
+import "../styles/playbrowser.css";
+import "../styles/template.css";
+import SinglePlay from "./SinglePlay.js";
+
 const PlayList = (props) => {
-    return (
-        <section className="playList">
-            <h2>List/Matches</h2>
-            <table>
-                <tr>
-                    <th>Title</th>
-                    <th>year</th>
-                    <th> </th>
-                </tr>
-                <tr>
-                    {/* this is an example */}
-                    <td>Hamlet</td>
-                    <td>1970</td>
-                    <td>
-                        <button>❤️</button>
-                        <button>View</button>
-                    </td>
-                </tr>
-            </table>
-
-
-        </section>
-
-
-
-    );
-}
+  return (
+    <section className="playList">
+      <h2>List/Matches</h2>
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>year</th>
+          <th className="viewbtn"> </th>
+          <th className="favbtn"> </th>
+        </tr>
+        <SinglePlay />
+      </table>
+    </section>
+  );
+};
 
 export default PlayList;
