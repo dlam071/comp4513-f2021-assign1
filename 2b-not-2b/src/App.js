cbe487253912d746327e16ee2be00434436b8952
dborc610 <dborc610@mtroyal.ca>
2021-11-04 00:06:25 -0600

    added fetch and state

--- 2b-not-2b/src/App.js
+++ 2b-not-2b/src/App.js
@@ -7,6 +7,16 @@ import PlayDetails from "./components/PlayDetails.js";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [plays, setPlays] = useState([]);
+  useEffect(() => {
+    const url =
+      "https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/list.php";
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => setPlays(data))
+      .catch((err) => console.error(err));
+  });
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
