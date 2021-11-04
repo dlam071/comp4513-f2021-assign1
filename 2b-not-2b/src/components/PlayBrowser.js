0d37d4671e5c25466b3a5057e6da2b277386842b
halna399 <halna399@mtroyal.ca>
2021-11-04 01:45:51 -0600

    imported style css and ya

--- 2b-not-2b/src/components/PlayBrowser.js
+++ 2b-not-2b/src/components/PlayBrowser.js
@@ -3,14 +3,16 @@ import Header from "./Header";
 import Favorites from "./Favorites";
 import PlayFilters from "./PlayFilters";
 import PlayList from "./PlayList";
-
+import "../styles/playbrowser.css";
 const PlayBrowser = (props) => {
   return (
-    <section className="PlayBrowser">
+    //css and html tags are not set for this im just using it as an example ty and goodbye
+    <section className="playBrowser">
       <Header />
       <Favorites />
       <PlayFilters />
       <PlayList />
+
     </section>
   );
 };
