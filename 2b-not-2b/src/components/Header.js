535dc487c405e48f5e4c00f892fc7df1a5d4e6d3
Darren Lam <dlam071@mtroyal.ca>
2021-11-04 18:21:50 -0600

    About modal and styles framework created

--- 2b-not-2b/src/components/Header.js
+++ 2b-not-2b/src/components/Header.js
@@ -1,12 +1,28 @@
 import React from "react";
+import Modal from "react-modal";
 import "../styles/playbrowser.css";
 import About from "./About.js";
 
+Modal.setAppElement(document.getElementById('#appHeader'));
+
 const Header = (props) => {
+
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const setModal = () => setIsOpen(!isOpen);
+
+  const handleClick = () => {
+    console.log("about here");
+    setModal();
+  }
+
   return (
-    <nav className="header">
+    <nav className="header" id='appHeader'>
       <p>Play Browser</p>
-      <button className="buttonThin buttonSolid">About</button>
+      <button className="buttonThin buttonSolid" onClick={handleClick}>About</button>
+      {isOpen ? <About
+             onRequestClose={setModal}
+             contentLabel="About Modal" /> : null}
     </nav>
   );
 };
