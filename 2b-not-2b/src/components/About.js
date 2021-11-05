0097eb9c15091d70d55400a58a83f7ee52ea32e7
Darren Lam <dlam071@mtroyal.ca>
2021-11-04 20:59:05 -0600

    shadow box added to about modal.

--- 2b-not-2b/src/components/About.js
+++ 2b-not-2b/src/components/About.js
@@ -4,32 +4,33 @@ import "../styles/About.css";
 
 const About = (props) => {
   return (
-    <div id="aboutBox">
-      <ul>
-        <li>
-          Heart Logos from
-          <a href="https://fonts.google.com/icons?selected=Material+Icons+Outlined:assignment_returned&icon.query=heart">
-            Google Icons
-          </a>
-        </li>
-        <li>
-          Some button Styling
-          <a href="https://moderncss.dev/css-button-styling-guide/">from</a>
-        </li>
-        <li>
-          <a href="link">LINK</a>
-        </li>
-        <li>
-          <a href="link">LINK</a>
-        </li>
-        <li>
-          <a href="link">LINK</a>
-        </li>
-        <li>
-          <a href="link">LINK</a>
-        </li>
-      </ul>
-    </div>
+      
+      <div id="aboutBox">
+        <ul>
+          <li>
+            Heart Logos from
+            <a href="https://fonts.google.com/icons?selected=Material+Icons+Outlined:assignment_returned&icon.query=heart">
+              Google Icons
+            </a>
+          </li>
+          <li>
+            Some button Styling
+            <a href="https://moderncss.dev/css-button-styling-guide/">from</a>
+          </li>
+          <li>
+            <a href="link">LINK</a>
+          </li>
+          <li>
+            <a href="link">LINK</a>
+          </li>
+          <li>
+            <a href="link">LINK</a>
+          </li>
+          <li>
+            <a href="link">LINK</a>
+          </li>
+        </ul>
+      </div>
   );
 };
 
