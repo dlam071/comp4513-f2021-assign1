9c3ffc590ac886a2f97f9ecd07100721bbcbfc3f
halna399 <halna399@mtroyal.ca>
2021-11-06 03:33:03 -0600

    sort title and sort date

--- 2b-not-2b/src/components/PlayList.js
+++ 2b-not-2b/src/components/PlayList.js
@@ -1,16 +1,46 @@
 import React from "react";
+import { useState } from "react";
 import "../styles/playbrowser.css";
 import "../styles/template.css";
 import SinglePlay from "./SinglePlay.js";
 
 const PlayList = (props) => {
+
+  const [title, setTitle] = useState([]);
+  const [clicked, setClicked] = useState(false);
+  const [date, setDate] = useState([]);
+  const sortTitle = () => {
+    let sortedByTitle = [...props.plays];
+    clicked ? setTitle(sortedByTitle) : setTitle(sortedByTitle.sort((a, b) => {
+      if (a.title > b.title)
+        return 1;
+      else
+        return -1
+    }));
+  }
+
+  const sortDate = () => {
+    let sortedByDate = [...props.plays];
+    clicked ? setDate(sortedByDate) : setDate(sortedByDate.sort((a, b) => {
+      if (a.likelyDate > b.likelyDate)
+        return 1;
+      else
+        return -1
+    }));
+  }
+
+  const handleClick = () => {
+    clicked ? setClicked(false) : setClicked(true);
+  }
+
+
   return (
     <section className="playList">
       <h2>List/Matches</h2>
       <table>
         <tr className="listHeader">
-          <th className="listTitle">Title</th>
-          <th className="listYear">Year</th>
+          <th className="listTitle" onClick={() => { handleClick(); sortTitle(); }}>Title</th>
+          <th className="listYear" onClick={() => { handleClick(); sortDate(); }}>Year</th>
           <th className="viewbtn"> </th>
           <th className="favbtn"> </th>
         </tr>
