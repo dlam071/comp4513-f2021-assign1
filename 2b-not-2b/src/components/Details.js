bd14db18f71000d42eb26f52aff92a40d1c4ec45
halna399 <halna399@mtroyal.ca>
2021-11-06 20:36:18 -0600

    the details page started populating stuff woo

--- 2b-not-2b/src/components/Details.js
+++ 2b-not-2b/src/components/Details.js
@@ -1,42 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
-import "../styles/Details.css";
-import Favorites from "./Favorites";
-
+// import Favorites from "./Favorites";
+import Characters from "./Characters.js";
 
 const Details = (props) => {
+
+  const [info, setInfo] = useState([]);
+
+  useEffect(() => {
+    let s = props.play.filename;
+    s = s.substring(0, s.lastIndexOf(".")); //removes the .json extension in the filename
+    const url = "https://www.randyconnolly.com//funwebdev/3rd/api/shakespeare/play.php?name=" + s;
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        setInfo(data)
+      })
+
+  }, [])
+console.log(props.play)
+
+
   return (
     <section className="playDetails">
       <Header />
-      <Favorites
+      {/* <Favorites
+        updateFavorites={props.updateFavorites}
         favorites={props.favorites}
         plays={props.plays}
-      />
+      /> */}
       <div className="synopsis">
-        <h2>Play Title</h2>
-        <p>Play Synopsis</p>
+        <h2>{info.title}</h2>
+        <p>Synopsis</p>
       </div>
       <div className="detailItems">
         <div className="playDetails">
-          <p>Composition</p>
-          <p>Genre</p>
-          <p>Wiki Link</p>
-          <p>Gutenberg Link</p>
-          <p>Shakespeare.org Link</p>
-          <p>Description</p>
+          <p>Composition: {props.play.likelyDate}</p>
+          <p>Genre: {props.play.genre}</p>
+          <a href={props.play.wiki}>Wiki</a>
+          <a href={props.play.gutenberg}>Gutenberg</a>
+          <a href={props.play.shakespeareOrg}>Shakespeare Org</a>
+          <p>Description: {props.play.desc}</p>
         </div>
         <div className="playCharacters">
-          <p>Character Name</p>
+        {/* <Characters chars={info.persona} /> */}
         </div>
-        <div className="playText">
-          <p>Text</p>
-        </div>
-      </div>
-      <div className="closeButton">
-        <Link to="/browse">
-          <button className="button">Close</button>
-        </Link>
       </div>
     </section>
   );
