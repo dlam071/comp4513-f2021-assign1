7d897c979c0f419dccb854b64b9e99e2d6423123
Darren Lam <dlam071@mtroyal.ca>
2021-11-07 20:45:20 -0700

    Framework for textFilter

--- 2b-not-2b/src/components/TextForm.js
+++ 2b-not-2b/src/components/TextForm.js
@@ -1,14 +1,39 @@
 import React from "react";
+import Characters from "./Characters";
 
 const TextForm = (props) => {
-console.log(props.toggleReadText)
-  return (
-    <section className="textForm">
-      <p>TextForm</p>
+  
+    return(
+      <section className="textForm">
+        <p>TextForm</p>
 
-      <button className="button" onClick={props.toggleReadText}>Close</button>
-    </section>
-  );
+        <form className="form">
+          <div className="form">
+            <label id="act">Act</label>
+            <select name="act" /*onClick={handleActChange}*/>
+              <option value="">Select an Act</option>
+            </select>
+            <label id="scene">Scene</label>
+            <select name="scene" /*onClick={handleSceneChange}*/>
+              <option value="">Select a Scene</option>
+            </select>
+            <label id="character">Character</label>
+            <select name="character" /*onClick={handleCharacterChange}*/>
+              <option value="">Select a Character</option>
+            </select>
+
+            {/* <label id="searchTerm">Search
+            <input
+              type="text"
+              name="term"
+              className="searchTerm"
+            />
+    </label> */}
+          </div>
+        </form>
+        <button className="button" onClick={props.toggleReadText}>Close</button>
+      </section >
+    );
 }
 
 export default TextForm;
