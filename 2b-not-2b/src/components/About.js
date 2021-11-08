8d0d73681d137bb5ca7e06f9614baa5c08ffd453
Darren Lam <dlam071@mtroyal.ca>
2021-11-07 23:37:34 -0700

    About styles

--- 2b-not-2b/src/components/About.js
+++ 2b-not-2b/src/components/About.js
@@ -5,58 +5,93 @@ import "../styles/About.css";
 const About = (props) => {
   return (
     <div className="aboutBox">
-      <ul>
-        <li>
-          Heart Logos from
-          <a
-            target="_blank"
-            href="https://fonts.google.com/icons?selected=Material+Icons+Outlined:assignment_returned&icon.query=heart"
-          >
-            Google Icons
-          </a>
-        </li>
-        <li>
-          Some button Styling
-          <a
-            target="_blank"
-            href="https://moderncss.dev/css-button-styling-guide/"
-          >
+      <div className="assignInfo">
+        <h2>Assignment 1</h2>
+        <h3>COMP 4513: Randy Connolly</h3>
+        <p>Designed by: Hatoon Al-Nakshabandi, Dylan Borchert, and Darren Lam</p>
+      </div>
+      <div className="lists">
+        <ul classNames="techUsed">
+          <li>
+            React
+          </li>
+          <li>
+            React-DOM
+          </li>
+          <li>
+            React-modal
+          </li>
+          <li>
+            React-router
+          </li>
+          <li>
+            React-router-DOM
+          </li>
+          <li>
+            React-tabs
+          </li>
+          <li>
+            lodash
+          </li>
+          <li>
+            Style-components
+          </li>
+        </ul>
+        <ul className="sources">
+          <li>
+            Heart Logos from
+            <a
+              target="_blank"
+              href="https://fonts.google.com/icons?selected=Material+Icons+Outlined:assignment_returned&icon.query=heart"
+            >
+              Google Icons
+            </a>
+          </li>
+          <li>
+            Some button Styling
+            <a
+              target="_blank"
+              href="https://moderncss.dev/css-button-styling-guide/"
+            >
+              from
+            </a>
+          </li>
+          <li>
+            Icons made by
+            <a target="_blank" href="https://www.freepik.com" title="Freepik">
+              Freepik
+            </a>
             from
-          </a>
-        </li>
-        <li>
-          Icons made by
-          <a target="_blank" href="https://www.freepik.com" title="Freepik">
-            Freepik
-          </a>
-          from
-          <a target="_blank" href="https://www.flaticon.com/" title="Flaticon">
-            www.flaticon.com
-          </a>
-        </li>
-        <li>
-          Scroll bar styles from
-          <a
-            target="_blank"
-            href="https://www.w3schools.com/howto/howto_css_custom_scrollbar.asp"
-          >
-            here
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="link">
-            LINK
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="link">
-            LINK
-          </a>
-        </li>
-      </ul>
-      <button className="buttonSolid buttonThin" onClick={props.onRequestClose}>
-        Close
-      </button>
+            <a target="_blank" href="https://www.flaticon.com/" title="Flaticon">
+              www.flaticon.com
+            </a>
+          </li>
+          <li>
+            Scroll bar styles from
+            <a
+              target="_blank"
+              href="https://www.w3schools.com/howto/howto_css_custom_scrollbar.asp"
+            >
+              here
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="link">
+              LINK
+            </a>
+          </li>
+          <li>
+            <a target="_blank" href="link">
+              LINK
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="close">
+        <button className="buttonSolid buttonThin" onClick={props.onRequestClose}>
+          Close
+        </button>
+      </div>
     </div>
   );
 };
