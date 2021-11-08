c984482aea9e39b8bc159653929540f3b9397fbb
halna399 <halna399@mtroyal.ca>
2021-11-07 21:22:00 -0700

    script done and made a speech component woo

--- 2b-not-2b/src/components/ScriptText.js
+++ 2b-not-2b/src/components/ScriptText.js
@@ -1,7 +1,40 @@
 import React from "react";
+import Scene from "./Scene";
 
 const ScriptText = (props) => {
-    return (<p>ScriptText</p>);
+    console.log(props.info.acts);
+    let play = props.info;
+
+
+    const sceneStuff = (act) => {
+        return act.scenes.title;
+    }
+
+
+    return (
+        <section className="">
+            <h1>{play.title}</h1>
+            {
+                play.acts.map(act => {
+                    return (
+                        <div>
+                            <h2>{act.name}</h2>
+                            <Scene act={act} />
+                        </div>
+                    )
+
+                })
+            }
+
+
+        </section>
+
+
+
+
+
+
+    );
 }
 
 export default ScriptText;
