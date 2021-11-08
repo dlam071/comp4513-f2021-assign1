38b70ea09c54ddf21a3a2d3a27288744458fe932
Darren Lam <dlam071@mtroyal.ca>
2021-11-07 22:36:39 -0700

    hell... hell on earth. (getting scenes from the acts)

--- 2b-not-2b/src/components/TextForm.js
+++ 2b-not-2b/src/components/TextForm.js
@@ -1,13 +1,14 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import Characters from "./Characters";
 
 const TextForm = (props) => {
 
   const [act, setAct] = useState();
+  const [scene, setScene] = useState();
+  const [character, setCharacter] = useState();
 
   const generateActs = () => {
-    console.log(props.text);
-    let acts = props.text.map((n) => n.name);
+    let acts = props.text.map(n => n.name);
     let generatedActs = acts.filter(
       (item, index) => acts.indexOf(item) === index
     );
@@ -18,42 +19,99 @@ const TextForm = (props) => {
     ));
   };
 
-  const handleActChange = (e) => setAct(e.target.value);
+  const generateScenes = () => {
+    console.log("woo")
+    if(act) {
+      let chosen = props.text.find(t => act == t.name);
+    console.log( chosen);
+      console.log(props.text[act])
+      let scenes = chosen.scenes.map(n => n.name);
+      let generatedActs = scenes.filter(
+        (item, index) => scenes.indexOf(item) === index
+      );
+      return generatedActs.map((n) => (
+        <option defaultValue={scenes} onClick={handleSceneChange}>
+          {n}
+        </option>
+      ));
 
-    return(
-      <section className="textForm">
-        <p>TextForm</p>
+    } else {
+      return null;
+    }
 
-        <form className="form">
-          <div className="form">
-            <label id="act">Act</label>
-            <select name="act" /*onClick={handleActChange}*/>
-              <option value="">Select an Act</option>
-              {generateActs()}
-            </select>
+  };
 
-            <label id="scene">Scene</label>
-            <select name="scene" /*onClick={handleSceneChange}*/>
-              <option value="">Select a Scene</option>
-            </select>
+  // const generateScenes = () => {
+  //   console.log(props.scenes);
 
-            <label id="character">Character</label>
-            <select name="character" /*onClick={handleCharacterChange}*/>
-              <option value="">Select a Character</option>
-            </select>
+  //   let scenes = props.text.map(a => {
+  //     return a.scenes.map(s => {
+  //       console.log(s.name);
+  //       return s.name;
+  //     });
+  //   })
 
-            {/* <label id="searchTerm">Search
+  //   return (
+  //     scenes.map(p => {
+  //       return (<option defaultValue={p} onClick={handleSceneChange}>
+  //         {p}
+  //       </option>
+  //       )
+  //     }))
+
+
+
+  //   // let scenes = act.scenes.map(n => n.name);
+  //   // let generatedScenes = scenes.filter(
+  //   //   (item, index) => scenes.indexOf(item) === index
+  //   // );
+  //   // return generatedScenes.map(n => (
+  //   //   <option defaultValue={scenes} onClick={handleSceneChange}>
+  //   //     {scenes}
+  //   //   </option>
+  //   // ));
+  // };
+
+  const handleActChange = (e) => {
+    setAct(e.target.value);
+  }
+  const handleSceneChange = (e) => setScene(e.target.value);
+
+  return (
+    <section className="textForm">
+      <p>TextForm</p>
+
+      <form className="form">
+        <div className="form">
+          <label id="act">Act</label>
+          <select name="act" onClick={handleActChange}>
+            <option value="">Select an Act</option>
+            {generateActs()}
+          </select>
+
+          <label id="scene">Scene</label>
+          <select name="scene" /*onClick={handleSceneChange}*/>
+            <option value="">Select a Scene</option>
+            {generateScenes()}
+          </select>
+
+          <label id="character">Character</label>
+          <select name="character" /*onClick={handleCharacterChange}*/>
+            <option value="">Select a Character</option>
+          </select>
+
+          {/* <label id="searchTerm">Search
             <input
               type="text"
               name="term"
               className="searchTerm"
             />
     </label> */}
-          </div>
-        </form>
-        <button className="button" onClick={props.toggleReadText}>Close</button>
-      </section >
-    );
+        </div>
+      </form>
+      <button className="button" onClick={props.toggleReadText}>Close</button>
+    </section >
+  );
 }
 
 export default TextForm;
