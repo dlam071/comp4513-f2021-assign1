c220cb4f5fa3ade9529d0339f79dd20c5216af07
halna399 <halna399@mtroyal.ca>
2021-12-04 18:59:40 -0700

    added

--- 2b-not-2b/src/list-server.js
+++ 2b-not-2b/src/list-server.js
@@ -1,13 +1,21 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
-const opt = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-};
+const connect = () => {
+    const opt = {
+        useUnifiedTopology: true,
+        useNewUrlParser: true, 
+        dbName: '2b-not-2b'
+    };
 
-mongoose.connect(process.env.MONGO_URL, opt);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', () => {
-    console.log("connected to mongo");
-})
+    mongoose.connect(process.env.MONGO_URL, opt);
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', () => {
+        console.log("connected to mongo");
+    });
+}
+
+module.exports = {
+    connect
+}
+
