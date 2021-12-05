abc5ea6a1852e6db2942c9f70db8a993d7c713c3
halna399 <halna399@mtroyal.ca>
2021-12-04 19:12:27 -0700

    connected to mongoose... again

--- 2b-not-2b/src/list-server.js
+++ 2b-not-2b/src/list-server.js
@@ -4,5 +4,9 @@ const express = require('express');
 const app = express();
 
 //create connection to database
-require('./handlers/dataConnector')
+require('./handlers/dataConnector.js').connect();
 
+const port = process.env.port;
+app.listen(port, () => {
+ console.log("Server running at port= " + port);
+}); 
