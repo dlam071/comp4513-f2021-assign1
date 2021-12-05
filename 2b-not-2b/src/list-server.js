2191a758ec15b83331dabe5b71a81f9476eeff76
halna399 <halna399@mtroyal.ca>
2021-12-04 19:02:25 -0700

    moved dataconnector

--- 2b-not-2b/src/list-server.js
+++ 2b-not-2b/src/list-server.js
@@ -1,21 +1,8 @@
-const mongoose = require('mongoose');
 require('dotenv').config();
-const connect = () => {
-    const opt = {
-        useUnifiedTopology: true,
-        useNewUrlParser: true, 
-        dbName: '2b-not-2b'
-    };
 
-    mongoose.connect(process.env.MONGO_URL, opt);
-    const db = mongoose.connection;
-    db.on('error', console.error.bind(console, 'connection error:'));
-    db.once('open', () => {
-        console.log("connected to mongo");
-    });
-}
+const express = require('express');
+const app = express();
 
-module.exports = {
-    connect
-}
+//create connection to database
+require('./handlers/dataConnector')
 
