362f05dd973a18aab30e531594751113826326f4
dborc610 <dborc610@mtroyal.ca>
2021-12-04 22:32:57 -0700

    chnages for hosting

--- 2b-not-2b/Server/list-server.js
+++ 2b-not-2b/Server/list-server.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //get our data model
-const Play = require('./models/List.js');
+const Play = require("./models/List.js");
 
-//to tell node to use json and http header features 
+//to tell node to use json and http header features
 app.use(express.urlencoded({ extended: true }));
 
 //use route handlers
-const listRouter = require('./handlers/listRouter.js');
+const listRouter = require("./handlers/listRouter.js");
 listRouter.handleAllPlays(app, Play);
 
 //create connection to database
-require('./handlers/dataConnector.js').connect();
+require("./handlers/dataConnector.js").connect();
 
-const port = process.env.port;
+const port = process.env.port || 8080;
 app.listen(port, () => {
-    console.log("Server running at port= " + port);
+  console.log("Server running at port= " + port);
 });
