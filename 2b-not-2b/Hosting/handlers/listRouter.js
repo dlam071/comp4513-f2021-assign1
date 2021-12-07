21e57e87adf36a4e8e48b9788177ac71bb577fff
halna399 <halna399@mtroyal.ca>
2021-12-07 15:53:59 -0700

    list router restored

--- 2b-not-2b/Hosting/handlers/listRouter.js
+++ 2b-not-2b/Hosting/handlers/listRouter.js
@@ -2,7 +2,7 @@
 const handleAllPlays = (app, Play) => {
     app.route('/api/list')
         .get((req, resp) => {
-            Play.find({},"-playText", (err, data) => {
+            Play.find({}, "-playText", (err, data) => {
                 if (err) {
                     resp.json({ message: 'Unable to connect to plays' });
                 } else {
@@ -12,6 +12,23 @@ const handleAllPlays = (app, Play) => {
         })
 }
 
+
+//get play by id
+handlePlayByID = (app, Play) => {
+    app.route('/api/play/:id')
+        .get((req, resp) => {
+            Play.find({ id: req.params.id }, "-playText", (err, data) => {
+                if (err) {
+                    resp.json({ message: 'Play not found :(' });
+                } else {
+                    resp.json(data);
+                }
+            })
+        })
+}
+
+
 module.exports = {
-    handleAllPlays
+    handleAllPlays,
+    handlePlayByID
 };
