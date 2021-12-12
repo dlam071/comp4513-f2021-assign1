0e76d6af85930aed5dc524e4514c443b5d72e26c
Darren Lam <dlam071@mtroyal.ca>
2021-12-11 20:19:33 -0700

    yeeeeet

--- 2b-not-2b/Development/src/components/Header.js
+++ 2b-not-2b/Development/src/components/Header.js
@@ -112,22 +112,15 @@ const Header = (props) => {
             xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#2e4b8aff"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M11 7h2v2h-2zm0 4h2v6h-2zm1-9C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/>
       
       </svg>
-          {/* <Button
-            type="primary"
-            className="buttonThin buttonSolid"
-            onClick={showAboutDrawer}
-          >
-            {" "}
-            About{" "}
-          </Button> */}
+
           <Drawer
             title="About"
             placement="top"
             closable={true}
             onClose={onAboutClose}
             visible={visibleAbout}
-            size="large"
-          >
+            height="800"
+    >
             <About />
           </Drawer>
         </div>
